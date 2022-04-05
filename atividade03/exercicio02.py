51dde1dbbc4d4d4c3a7abd7fc6526ad40c95ad77
Fábio Reis <espynho23@gmail.com>
2022-04-05 07:42:16 -0300

    feat: exercício02

---
+++ atividade03/exercicio02.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+valor = float(input("Entre com um número: "))
+
+if valor >= 0:
+    print("O valor digitado,", valor,"é positivo")
+else:
+    print("O valor digitado,", valor,"é negativo")
