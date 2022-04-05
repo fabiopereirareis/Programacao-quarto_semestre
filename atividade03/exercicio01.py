1309cbe0b7998110e45b384075eceb71a1adb9d2
Fábio Reis <espynho23@gmail.com>
2022-04-05 07:33:55 -0300

    feat: exercício01

---
+++ atividade03/exercicio01.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+numero01 = input("Digite o valor do primeiro número: ")
+numero02 = input("Digite o valor do segundo número: ")
+
+if numero01 > numero02:
+    print("O número01,", numero01, "é maior que o número02", numero02)
+else:
+    print("O número02,", numero02, "é maior que o número01", numero01)
